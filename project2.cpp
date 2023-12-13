9cdaec95d7a1c27603221a415004c93bb177dc21
David Antunes <davaantunes@gmail.com>
2023-12-13 23:12:58 +0000

    dfs no struct

--- project2.cpp
+++ project2.cpp
@@ -26,21 +26,19 @@ class Graph {
             _adjList[v].push_back(w);
         }
 
-        void DFS(int start) {
+        void DFS(int startVertex) {
             unordered_set<int> visited;
-            stack<Vertex> stack;
-            Vertex startVertex = {._id = start, ._parent = -1};
+            stack<int> stack;
             stack.push(startVertex);
             while (!stack.empty()) {
-                Vertex currentVertex = stack.top();
+                int currentVertex = stack.top();
                 stack.pop();
-                if (visited.find(currentVertex._id) == visited.end()) {
-                    visited.insert(currentVertex._id);
-                    printf("Visited %d\n", currentVertex._id);
-                    for (int neighbor : _adjList[currentVertex._id]) {
+                if (visited.find(currentVertex) == visited.end()) {
+                    visited.insert(currentVertex);
+                    printf("Visited %d\n", currentVertex);
+                    for (int neighbor : _adjList[currentVertex]) {
                         if (visited.find(neighbor) == visited.end()) {
-                            Vertex vertex = {._id = neighbor, ._parent = currentVertex._id};
-                            stack.push(vertex);
+                            stack.push(neighbor);
                         }
                     }
                 }
