4bd9c9077b19a8fec16c72404918c5af3cecc7d1
David Antunes <davaantunes@gmail.com>
2023-12-17 16:07:49 +0000

    no prints, 1 wrong answer, 11 accepted, 650

--- project2.cpp
+++ project2.cpp
@@ -21,7 +21,6 @@ class Graph {
         }
         
         void collapse(unordered_set<int> SCC) {
-            printf("entrei\n");
             int newVertex = 0;
             for (int i = 1; i <= _numVertices; i++) {
                 if (SCC.find(i) != SCC.end()) {
@@ -31,7 +30,6 @@ class Graph {
                 }
             }
             for (int del : SCC) {
-                 printf("%d\n", del);
                 for (int v : _adjList[del]) {
                     if (v != newVertex && SCC.find(v) != SCC.end()) {
                         _adjList[newVertex].insert(v);
@@ -56,28 +54,18 @@ class Graph {
         }
 
         int DFS(int startVertex, unordered_set<int>* visited, stack<int>* endOrder) {
-            vector<int> distance(_numVertices+1);
             stack<int> stack;
             unordered_set<int> ended;
             int maxJumps = 0;
-            distance[startVertex] = 0;
             stack.push(startVertex);
-            printf("DFS em %d\n", startVertex);
             while (!stack.empty()) {
                 int currentVertex = stack.top();
                 int end = 1;
                 if (visited->find(currentVertex) == visited->end()) {
-                    printf("visitei %d\n", currentVertex);
                     visited->insert(currentVertex);
                     for (int neighbor : _adjList[currentVertex]) {
-                        distance[neighbor] = max(distance[currentVertex] + 1, distance[neighbor]);
-                        printf("dist %d = %d\n",neighbor, distance[neighbor]);
-                        if (distance[neighbor] > maxJumps) {
-                            maxJumps = distance[neighbor];
-                        }
                         if (visited->find(neighbor) == visited->end()) {
                             stack.push(neighbor);
-                            printf("encontrei %d\n", neighbor);
                             end = 0;
                         }
                     }
@@ -85,7 +73,6 @@ class Graph {
                 if (end && ended.find(currentVertex) == ended.end()) {
                     ended.insert(currentVertex);
                     endOrder->push(currentVertex);
-                    printf("terminei %d\n", currentVertex);
                     stack.pop();
                 }
                 else if (end) {
@@ -103,22 +90,18 @@ class Graph {
             int maxJumps = 0;
             distance[startVertex] = 0;
             stack.push(startVertex);
-            printf("DFS em %d\n", startVertex);
             while (!stack.empty()) {
                 int currentVertex = stack.top();
                 stack.pop();
                 if (visited->find(currentVertex) == visited->end()) {
-                    printf("visitei %d\n", currentVertex);
                     visited->insert(currentVertex);
                 }
                 for (int neighbor : _adjList[currentVertex]) {
                     distance[neighbor] = max(distance[currentVertex] + 1, distance[neighbor]);
-                    printf("dist %d = %d\n",neighbor, distance[neighbor]);
                     if (distance[neighbor] > maxJumps) {
                         maxJumps = distance[neighbor];
                     }
                     stack.push(neighbor);
-                    printf("encontrei %d\n", neighbor);
                 }
             }
             return maxJumps;
